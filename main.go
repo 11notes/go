a2ad8813789291d8dacafb12acffdfef87041673
ElevenNotes <github@11notes.ch>
2025-08-25 23:46:35 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/11notes/go/util"
+)
+
+func main(){
+	util.Log("inf", "my little message")
+}
---
+++ util/util.go
@@ -0,0 +1,23 @@
+// analog to docker-util for shell and javascript in go
+package util
+
+import (
+	"fmt"
+	"time"
+	"regexp"
+	"os"
+)
+
+// output log in json format with time stamp and simple string message
+func Log(t string, m string){
+	l := "INF"
+	switch {
+		case regexp.MustCompile(`(?i)DEBUG|DBG|DEB`).MatchString(t): l = "DBG"
+		case regexp.MustCompile(`(?i)INFO|INF|IN`).MatchString(t): l = "INF"
+		case regexp.MustCompile(`(?i)WARNING|WARN|WRN`).MatchString(t): l = "WRN"
+		case regexp.MustCompile(`(?i)ERROR|ERR`).MatchString(t): l = "ERR"
+		case regexp.MustCompile(`(?i)START`).MatchString(t): l = fmt.Sprintf("starting %s v%s", os.Getenv("APP_NAME"), os.Getenv("APP_VERSION"))
+		case regexp.MustCompile(`(?i)PATCH|FIX`).MatchString(t): l = "FIX"
+	}
+	fmt.Printf(`{"time":"%s","type":"%s","msg":"%s"}` + "\n", time.Now().Format("2006-01-02T15:04:05.000Z"), l, m)
+}
