151c0238c1123c1e1ef9ddb741049a6f58ff41ac
ElevenNotes <github@11notes.ch>
2025-10-13 10:03:54 +0200

    fix: wrong parameter set

--- util/util.go
+++ util/util.go
@@ -18,7 +18,7 @@ func (c *Util) Log(t string, m string){
 		case regexp.MustCompile(`(?i)INFO|INF|IN`).MatchString(t): l = "INF"
 		case regexp.MustCompile(`(?i)WARNING|WARN|WRN`).MatchString(t): l = "WRN"
 		case regexp.MustCompile(`(?i)ERROR|ERR`).MatchString(t): l = "ERR"
-		case regexp.MustCompile(`(?i)START`).MatchString(t): l = fmt.Sprintf("starting %s v%s", os.Getenv("APP_NAME"), os.Getenv("APP_VERSION"))
+		case regexp.MustCompile(`(?i)START`).MatchString(t): m = fmt.Sprintf("starting %s v%s", os.Getenv("APP_NAME"), os.Getenv("APP_VERSION"))
 		case regexp.MustCompile(`(?i)PATCH|FIX`).MatchString(t): l = "FIX"
 	}
 	fmt.Printf(`{"time":"%s","type":"%s","msg":"%s"}` + "\n", time.Now().Format("2006-01-02T15:04:05.000Z"), l, m)
